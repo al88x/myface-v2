ce63efbcd58bc9486a997b7450204bc3ac8ed8bc
Alexandru Catarau <alexctrx@gmail.com>
2019-11-18 13:49:51 +0000

    Added navbar

---
+++ src/component/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import '../style/App.scss';
+import {Navbar} from "./Navbar";
+import {Route, BrowserRouter as Router, Switch} from "react-router-dom";
+import {Posts} from "./Posts";
+import {Users} from "./Users";
+
+function App() {
+    return (
+        <div>
+            <header>
+                <Router>
+                    <div className="nav-bar">
+                    <Navbar/>
+                        <Switch>
+                            <Route path="/posts">
+                                <Posts />
+                            </Route>
+                            <Route path="/users">
+                                <Users />
+                            </Route>
+                        </Switch>
+                    </div>
+                </Router>
+
+            </header>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/component/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Link} from 'react-router-dom'
+import '../style/Navbar.scss';
+
+export function Navbar() {
+    return (
+        <nav>
+            <ul className="nav-bar-list">
+                <li>
+                    <Link to="/posts" className="nav-element">Posts</Link>
+                </li>
+                <li>
+                    <Link to="/users" className="nav-element">Users</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+}
