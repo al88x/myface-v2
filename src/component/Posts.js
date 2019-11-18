f88ff5116f9c7dfca488fd2d1a6e6649b839e130
Alexandru Catarau <alexctrx@gmail.com>
2019-11-18 16:41:40 +0000

    List posts from api

--- src/component/Posts.js
+++ src/component/Posts.js
@@ -1,5 +1,28 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 export function Posts(){
-    return <p>Posts</p>;
-}
+
+    const [hasError, setErrors] = useState(false);
+    const [posts, setPosts] = useState({});
+
+    async function fetchData() {
+        console.log("fetching data");
+        const res = await fetch('http://localhost:8080/api/posts');
+        res.json()
+            .then(res => setPosts(res))
+            .catch(err => setErrors(err));
+    }
+
+    useEffect(() => {
+        console.log("using the effect");
+        fetchData();
+    },[]);
+
+    return (
+        <div>
+            <span>{JSON.stringify(posts)}</span>
+            <hr/>
+            <span>Has error: {JSON.stringify(hasError)}</span>
+        </div>
+    );
+};
